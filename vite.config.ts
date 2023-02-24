2ff24231ff7b42d1d1eae7e55f815f802fd7124c
Birk Kensén <birk.kensen@gloot.com>
2023-02-24 17:52:45 +0100

    add aliases

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,18 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import { resolve } from 'path';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   server: {
     port: 3000,
+  },
+  resolve: {
+    alias: {
+      Components: resolve(__dirname, './src/components'),
+      Utils: resolve(__dirname, './src/utils'),
+      Queries: resolve(__dirname, './src/queries'),
+    },
   },
 });
