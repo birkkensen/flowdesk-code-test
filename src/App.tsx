0d7ba9fa1f735b2d26684de40d3ca76bc490c85a
Birk Kensén <birk.kensen@gloot.com>
2023-02-25 16:30:25 +0100

    put it all together on app

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,34 @@
-import { useGetAvailableCurrencies } from 'Queries/currencies';
+import { Navbar } from 'Components/navbar';
+import { CurrenyStats } from 'Components/currency-stats';
+import { useGetCurrencyTicker } from 'Queries/ticker';
+import { useState } from 'react';
+import { Form } from 'Components/form';
 
-function App() {
-  const { data } = useGetAvailableCurrencies();
-  console.log(data);
-  return <div>Bonjour le monde</div>;
-}
+const App = () => {
+  const [currencyPair, setCurrencyPair] = useState('');
+  const { data, isInitialLoading, isError } = useGetCurrencyTicker({
+    symbol: currencyPair,
+    enabled: !(currencyPair.length === 0),
+  });
+
+  const onSubmit = ({ symbol }: { symbol: string }) => {
+    setCurrencyPair(symbol);
+  };
+
+  return (
+    <div>
+      <Navbar />
+      <section className="p-16">
+        <h1 className="mb-8 text-4xl">Search for a currency pair</h1>
+        <Form onSubmit={onSubmit} />
+        <CurrenyStats
+          data={data}
+          isLoading={isInitialLoading}
+          isError={isError}
+        />
+      </section>
+    </div>
+  );
+};
 
 export default App;
