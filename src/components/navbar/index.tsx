8f8edfd78557e268f5a98f43941bf5c10b888a34
Birk Kensén <birk.kensen@gloot.com>
2023-02-25 15:48:20 +0100

    create navbar

---
+++ src/components/navbar/index.tsx
@@ -0,0 +1,7 @@
+export const Navbar = () => {
+  return (
+    <nav className="navbar bg-sky-800">
+      <a className="btn btn-ghost normal-case text-xl">flowdesk-code-test</a>
+    </nav>
+  );
+};
