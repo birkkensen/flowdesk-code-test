892211b8a71fe437b6d4c83dfbefd411d24687dd
Birk Kensén <birk.kensen@gloot.com>
2023-02-26 13:46:11 +0100

    add crypto name

--- src/components/currency-stats/components/base-quote-asset/index.tsx
+++ src/components/currency-stats/components/base-quote-asset/index.tsx
@@ -1,8 +1,10 @@
 import { StyledStat, StyledStats } from 'Components/currency-stats/styled';
 import { type ExchangeInfo } from 'Queries/currencies/types';
+import { cryptoSymbol } from 'crypto-symbol';
 
+const { nameLookup } = cryptoSymbol({});
 interface Props {
-  data?: ExchangeInfo;
+  data: ExchangeInfo;
 }
 
 export const BaseQuoteAsset: React.FC<Props> = ({ data }) => {
@@ -13,11 +15,17 @@ export const BaseQuoteAsset: React.FC<Props> = ({ data }) => {
         <div className="text-4xl font-extrabold text-slate-200">
           {data?.symbols[0].baseAsset}
         </div>
+        <div className="text-xs text-slate-400">
+          {nameLookup(data.symbols[0].baseAsset)}
+        </div>
       </StyledStat>
       <StyledStat className="border-0">
         <div className="text-slate-400">Quote asset</div>
         <div className="text-4xl font-extrabold text-slate-200">
           {data?.symbols[0].quoteAsset}
+        </div>
+        <div className="text-xs text-slate-400">
+          {nameLookup(data.symbols[0].quoteAsset)}
         </div>
       </StyledStat>
     </StyledStats>
